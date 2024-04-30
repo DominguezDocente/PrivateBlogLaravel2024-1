c40044f9d774837efd45d14e1324674194485668
Manuel Dominguez <dominguezguerrero1996@gmail.com>
2024-04-30 06:25:44 -0500

    ajuste sidebarmenu icono y sección activa

--- resources/views/layouts/sidebarMenu.blade.php
+++ resources/views/layouts/sidebarMenu.blade.php
@@ -1,17 +1,23 @@
+@php
+
+    $currentUrl = Request::url();
+
+@endphp
+
 <aside id="sidebar" class="sidebar">
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="{{ route('home.index') }}">
+        <a class="nav-link {{ $currentUrl != url('/') ? 'collapsed' : '' }}" href="{{ route('home.index') }}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('sections.index') }}">
-          <i class="bi bi-grid"></i>
+        <a class="nav-link {{ !str_contains($currentUrl, 'sections') ? 'collapsed' : '' }}" href="{{ route('sections.index') }}">
+          <i class="bi bi-puzzle"></i>
           <span>Secciones</span>
         </a>
       </li>
