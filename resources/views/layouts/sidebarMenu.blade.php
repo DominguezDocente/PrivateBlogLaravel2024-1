d8cf4f3847804d616958280991d89dfeddc3a14c
Alejandro Dominguez <alejo.triidy@gmail.com>
2024-05-13 23:26:13 -0500

    account

--- resources/views/layouts/sidebarMenu.blade.php
+++ resources/views/layouts/sidebarMenu.blade.php
@@ -8,26 +8,34 @@
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
-      <li class="nav-item">
-        <a class="nav-link {{ $currentUrl != url('/') ? 'collapsed' : '' }}" href="{{ route('home.index') }}">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link {{ $currentUrl != url('/') ? 'collapsed' : '' }}" href="{{ route('home.index') }}">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link {{ !str_contains($currentUrl, 'sections') ? 'collapsed' : '' }}" href="{{ route('sections.index') }}">
-          <i class="bi bi-puzzle"></i>
-          <span>Secciones</span>
-        </a>
-      </li>
+        @if(\App\Helpers\RoleHelper::isAuthorized('Secciones.showSections'))
+            <li class="nav-item">
+                <a class="nav-link {{ !str_contains($currentUrl, 'sections') ? 'collapsed' : '' }}" href="{{ route('sections.index') }}">
+                <i class="bi bi-puzzle"></i>
+                <span>Secciones</span>
+                </a>
+            </li>
+        @endif
 
-      <li class="nav-item">
-        <a class="nav-link {{ !str_contains($currentUrl, 'blogs') ? 'collapsed' : '' }}" href="{{ route('blogs.index') }}">
-          <i class="bi bi-card-text"></i>
-          <span>Blogs</span>
-        </a>
-      </li>
+        @if(\App\Helpers\RoleHelper::isAuthorized('Blogs.showBlogs'))
+            <li class="nav-item">
+                <a class="nav-link {{ !str_contains($currentUrl, 'blogs') ? 'collapsed' : '' }}" href="{{ route('blogs.index') }}">
+                <i class="bi bi-card-text"></i>
+                <span>Blogs</span>
+                </a>
+            </li>
+        @endif
+
+
+
+
 
     </ul>
 
